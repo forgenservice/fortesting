c23b86f2a7114dd43939b0c8a8fbb0c094dcc4ae
ithilellel-polunov <r.poluno@gmail.com>
2013-11-16 23:50:57 +0200

    Created BaseEntity

---
+++ Mission/src/org/hillel/it/mission/model/entity/BaseEntity.java
@@ -0,0 +1,15 @@
+package org.hillel.it.mission.model.entity;
+
+public abstract class BaseEntity {
+	int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+
+}
